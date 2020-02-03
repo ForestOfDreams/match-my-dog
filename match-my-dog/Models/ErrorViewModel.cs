35dad38b2539e871ace46bdeb790a599f1480695
ForestOfDreams <60626336+ForestOfDreams@users.noreply.github.com>
2020-02-04 00:35:11 +0300

    Добавьте файлы проекта.

---
+++ match-my-dog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace match_my_dog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
